dd0f32770a5136b186c64a1f3e6396a983d3e8d1
fahad-md-kamal <faahad.hossain@gmail.com>
2022-08-25 00:22:53 +0600

    map Data Structure configured

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fahad-md-kamal/go-dsa-75
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var m map[string]string
+	m := make(map[string]string)
+
+	m["name"] = "Muhammad"
+	// name := m["name"]
+	m["job"] = "Messanger of Allah"
+	m["calc"] = "5 + 5 = 10"
+
+
+	iterateMap(&m)
+	delete(m,"calc")
+	println("After DELETE of calc")
+
+	iterateMap(&m)
+
+	println("CHECK if key exists")
+	getMapKey(&m)
+
+	println("POPULATE map")
+	populateMapWithInitValues()
+
+}
+
+func getMapKey(m *map[string]string){
+	_, ok := (*m)["abc"]
+	fmt.Println(ok)
+}
+
+func iterateMap(m *map[string]string){
+	for key, value := range *m {
+		fmt.Printf("%s \t---->\t %s\n", key, value)
+	}
+	
+}
+
+func populateMapWithInitValues(){
+	commits := map[string]int{
+		"rsc": 3711,
+		"r":   2138,
+		"gri": 1908,
+		"adg": 912,
+	}
+	for k, v := range commits{
+		fmt.Printf("%v \t---> %v\n", k, v)
+	}
+}

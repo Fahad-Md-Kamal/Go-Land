db70c7436f133c934173d79626e5d1537f4f78da
fahad-md-kamal <faahad.hossain@gmail.com>
2022-08-26 01:42:50 +0600

    Two Sum Problem

--- main.go
+++ main.go
@@ -3,28 +3,48 @@ package main
 import "fmt"
 
 func main() {
-	// var m map[string]string
-	m := make(map[string]string)
+// var m map[string]string
+// m := make(map[string]string)
 
-	m["name"] = "Muhammad"
-	// name := m["name"]
-	m["job"] = "Messanger of Allah"
-	m["calc"] = "5 + 5 = 10"
+// m["name"] = "Muhammad"
+// // name := m["name"]
+// m["job"] = "Messanger of Allah"
+// m["calc"] = "5 + 5 = 10"
 
+// iterateMap(&m)
+// delete(m,"calc")
+// println("After DELETE of calc")
 
-	iterateMap(&m)
-	delete(m,"calc")
-	println("After DELETE of calc")
+// iterateMap(&m)
 
-	iterateMap(&m)
+// println("CHECK if key exists")
+// getMapKey(&m)
 
-	println("CHECK if key exists")
-	getMapKey(&m)
+// println("POPULATE map")
+// populateMapWithInitValues()
 
-	println("POPULATE map")
-	populateMapWithInitValues()
+	arr := []int{3, 4, 5, 6, 7}
+
+	result := twoSum(arr, 12)
+	fmt.Println(result)
 
 }
+
+
+func twoSum(arr []int, target int) []int{
+	m := make(map[int]int)
+	
+	for key, value := range arr{
+		if val, ok := m[target - value]; ok {
+			arr := []int{ val, key}
+			return arr
+		}
+
+		m[value] = key
+	}
+	return []int{-1,-1}
+}
+
 
 func getMapKey(m *map[string]string){
 	_, ok := (*m)["abc"]

b89ce4250ea5fc698002d12a8644ff410c97e733
fahad-md-kamal <faahad.hossain@gmail.com>
2022-08-26 18:31:49 +0600

    Valid Parenthesis solution

---
+++ stacks-n-queues/valid_parentheses.go
@@ -0,0 +1,37 @@
+package snq
+
+import (
+	"fmt"
+)
+
+func ValidParenthesis(s *string) bool {
+    var stack []string
+
+	m := map[string]string{
+		")" : "(",
+		"}" : "{",
+		"]" : "[",
+	}
+
+	for _, char := range *s {
+
+		if mapVal, ok := m[string(char)]; ok {
+
+			if len(stack) > 0{
+				length := len(stack) - 1
+				firstItem := stack[length]
+				stack = stack[:length]
+				if mapVal != firstItem{
+					fmt.Println("1st COND:\t", stack)
+					return false
+				}
+			}else{
+			   stack = append(stack, string(char))
+            }
+		} else {
+			stack = append(stack, string(char))
+		} 
+	}
+
+	return len(stack) == 0
+}
